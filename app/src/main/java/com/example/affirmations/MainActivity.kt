d6e3ef8247c45871e7c50a72e31b2a295ce986ad
sudipto <cg.sudipc@gmail.com>
2023-09-15 21:46:55 +0530

    created Affirmation class and Datasource class with loadAffirmations method that returns a list of affirmations

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // getting TextView reference using resource id
+        val textView: TextView = findViewById(R.id.textView)
+
+        /**
+         * getting the size of the affirmations list by calling Datasoirce.loadAffirmations(),
+         * converting it to a string and assigning it as the `text` of `textView`
+         * */
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.example.affirmations.model
+
+// data class with a parameterized constructor to take a string resource ID
+data class Affirmation(val stringResourceId : Int) {
+
+}
