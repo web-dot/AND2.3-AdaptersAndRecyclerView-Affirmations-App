7a544e370ac3aba0c8653a22480a36bbb351f896
sudipto <cg.sudipc@gmail.com>
2023-09-15 23:37:16 +0530

    created layout file for individual views in RecyclerView

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -10,13 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // getting TextView reference using resource id
-        val textView: TextView = findViewById(R.id.textView)
+        //val textView: TextView = findViewById(R.id.textView)
+        //textView.text = Datasource().loadAffirmations().size.toString()
 
-        /**
-         * getting the size of the affirmations list by calling Datasoirce.loadAffirmations(),
-         * converting it to a string and assigning it as the `text` of `textView`
-         * */
-        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
