47ac906813bae694b9d55cf9c8200dfd2c45d045
sudipto <cg.sudipc@gmail.com>
2023-09-17 00:41:46 +0530

    used ItemAdapter in MainActivity to display Affirmations in RecyclerView

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,9 @@ class MainActivity : AppCompatActivity() {
         //val textView: TextView = findViewById(R.id.textView)
         //textView.text = Datasource().loadAffirmations().size.toString()
 
+        val myDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -9,28 +9,42 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
+/**
+ * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data obejct
+ * */
 class ItemAdapter(
     val context : Context,
     private val dataSet : List<Affirmation>
     ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    // provide reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object
     class ItemViewHolder(private val view : View) : RecyclerView.ViewHolder(view) {
         val textView : TextView = view.findViewById(R.id.item_title)
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     * */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         /**
          * getting a reference of layout-inflater and inflating
          * the list item view. This adapterLayout holds a reference to the
          * list item view(from which we can later find child views like TextView)
-         *
          * */
+        // create a new view
         val adapterLayout = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.list_item, parent, false)
+
         return ItemViewHolder(adapterLayout)
     }
 
+    /**
+     * Replace the contents of a view (invoked by layout manager)
+     * */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataSet[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
